979172e70ef1af1a3cb6b9184fece3bf24cd1783
Raymond <imnotdacoco05@gmail.com>
2025-10-02 23:57:36 -0700

    Added API call logic

--- website/views.py
+++ website/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+
+def home(request):
+    # Check to see if logging in
+    if request.method == 'POST':
+        
+    return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    pass
